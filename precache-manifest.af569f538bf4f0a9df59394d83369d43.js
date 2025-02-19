3d5937bf4d5e36998e0d60f16ad176382cf5c0c8
karan <bamniyakaran71@gmail.com>
2025-02-19 14:14:11 +0000

    Updates

---
+++ precache-manifest.af569f538bf4f0a9df59394d83369d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f083918e8fab47a88f5b58251d17d32",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "3b2280994e1af0de761a",
+    "url": "/react-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "18b7bae5073816f5d31c",
+    "url": "/react-app/static/js/2.f49a9d45.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/react-app/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "3b2280994e1af0de761a",
+    "url": "/react-app/static/js/main.49e14428.chunk.js"
+  },
+  {
+    "revision": "f0a14034f6cf2522a56c",
+    "url": "/react-app/static/js/runtime~main.576b5930.js"
+  }
+]);
